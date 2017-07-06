29e137cd27b9aa9f096e96a99aeb9ffd9e4986a6
ridhwan <muhammadridhwandaud@gmail.com>
2017-07-06 15:47:33 +0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Router, Scene } from 'react-native-router-flux';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import PageOne from './PageOne';
+import PageTwo from './PageTwo';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Scene key="root">
+          <Scene key="pageOne" component={PageOne} title="PageOne" initial={true} />
+          <Scene key="pageTwo" component={PageTwo} title="PageTwo" />
+        </Scene>
+      </Router>
+    )
+  }
+}
+
+AppRegistry.registerComponent('router', () => App);
